26798520ab540bc0ade132bcbc82c0d277fdce67
Anitha Malaghan <147544944+Anitha-Malaghan@users.noreply.github.com>
2023-10-30 12:55:16 +0530

    Observer Design Pattern Added

---
+++ src/ObserverDesignPattern/EmailSender.java
@@ -0,0 +1,16 @@
+package ObserverDesignPattern;
+
+public class EmailSender implements  OrderPlacedSubscriber{
+    public EmailSender(){
+        Flipkart flipkart = Flipkart.getInstance();
+        flipkart.registerSubscriber(this);
+    }
+    @Override
+    public void announceOrderPlaced(){
+        sendEmail();
+
+    }
+    private void sendEmail(){
+        System.out.println("Email is sending");
+    }
+}
